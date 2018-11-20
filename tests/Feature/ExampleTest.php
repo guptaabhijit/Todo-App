03bd959fdaad5a7b63b65db93e1156ce5907bbad
abhijit <abhijith.lal@razorpay.com>
2018-11-20 11:52:11 +0530

    Basic Functional Test using GuzzleHttp

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class ExampleTest extends TestCase
 {
@@ -12,10 +15,55 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
+
+
+    private $client;
+
+    public function setUp()
+    {
+        //default IP for vagrant/homestead  192.168.10.10
+        //Use localhost if not running any virtual machine.
+        $this->client = new Client([
+            'base_uri'        => '192.168.10.10',
+            'exceptions' => false,
+        ]);
+
+        parent::setUp();
+    }
+
+
+
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->client->request(
+                'GET',
+                '192.168.10.10/todos'
+        );
 
-        $response->assertStatus(200);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+    }
+
+    public function testBasicErrorTest(){
+
+        $response = "";
+        try {
+
+            $response = $this->client->request(
+                'GET',
+                '192.168.10.10'
+            );
+        }catch(ClientException $e){
+            echo 'Uh oh! ' . $e->getMessage();
+        }
+
+        $this->assertEquals(400,$response->getStatusCode());
+    }
+
+
+
+    public function tearDown(){
+        $this->client = null;
     }
 }
